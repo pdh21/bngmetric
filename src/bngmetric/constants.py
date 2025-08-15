384a1435b7110a61d45893c63749efb9199f53d1
Peter Hurley <petedonhurley@gmail.com>
2025-08-15 20:33:52 +0100

    create the temporal lookup functions

--- src/bngmetric/constants.py
+++ src/bngmetric/constants.py
@@ -86,8 +86,8 @@ def _load_creation_table():
         if habitat_name in df_creation['Habitat Description'].values:
             # Get the row for this habitat from the loaded CSV, and extract the condition columns
             row_data = df_creation[df_creation['Habitat Description'] == habitat_name][condition_categories].iloc[0]
-            # Convert any "30+" strings to numeric 30
-            row_data = row_data.apply(lambda x: 30 if isinstance(x, str) and "30+" in x else x)
+            # Convert any "30+" strings to numeric 32
+            row_data = row_data.apply(lambda x: 32 if isinstance(x, str) and "30+" in x else x)
             ordered_creation_values.append(row_data.values)
         else:
             # If a habitat from master list is not in condition table, assume all 0s for conditions
@@ -100,6 +100,16 @@ def _load_creation_table():
 # Load the creation data and get the JAX-compatible matrix
 CREATION_MULTIPLIERS_MATRIX = _load_creation_table()
 
+def _load_temporal_multiplier():
+    """Loads the temporal multiplier from CSV and returns a JAX array."""
+    with pkg_resources.files(bngmetric_data).joinpath('temporal_multiplier_LUT.csv').open('r') as f:
+        df_temporal = pd.read_csv(f)
+    # Convert the 'Years' column to a JAX array of multipliers
+    return jnp.array(df_temporal.set_index('Year')['Time to target Multiplier'].values, dtype=jnp.float32)
+
+TEMPORAL_MULTIPLIER_LOOKUP = _load_temporal_multiplier()
+
+
 def _load_difficulty_multiplier():
     """Loads the difficulty multiplier from CSV and returns a dictionary for mapping."""
     with pkg_resources.files(bngmetric_data).joinpath('risk_multiplier.csv').open('r') as f:
@@ -110,9 +120,10 @@ def _load_difficulty_multiplier():
         if habitat_name in df_difficulty['Habitat Description'].values:
             # Get the row for this habitat from the loaded CSV, and extract the condition columns
             row_data = df_difficulty[df_difficulty['Habitat Description'] == habitat_name]['Multiplier'].iloc[0]
-            ordered_creation_values.append(row_data.values)
+            
+            ordered_creation_values.append(row_data)
             row_data = df_difficulty[df_difficulty['Habitat Description'] == habitat_name]['Multiplier.1'].iloc[0]
-            ordered_enhancement_values.append(row_data.values)
+            ordered_enhancement_values.append(row_data)
         else:
             # If a habitat from master list is not in condition table, assume all 0s for conditions
             # (or raise an error if this should be impossible)
@@ -123,4 +134,92 @@ def _load_difficulty_multiplier():
     return ordered_creation_values, ordered_enhancement_values
 
 CREATION_RISK,ENHANCEMENT_RISK = _load_difficulty_multiplier()
+
+
+#--load the temporal multiplier for enhancement
+def _load_temporal_enhancement_multiplier():
+    """Loads the temporal multiplier for enhancement from CSV and returns a JAX array."""
+    with pkg_resources.files(bngmetric_data).joinpath('enhancement_multiplier.csv').open('r') as f:
+        df_temporal_enhance = pd.read_csv(f)
+
+    ## ----Same distinctiveness values----
+    same_distinct=df_temporal_enhance[df_temporal_enhance.columns[[0,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,]]]
+    same_distinct = same_distinct.drop(index=[1], inplace=False)
+
+    # Create a dictionary mapping for column renaming
+    column_mapping = {}
+    for col in same_distinct.columns:
+        # Handle columns with dots by extracting the base name
+        if '.' in col:
+            base_name = col.split('.')[0]
+            column_mapping[col] = base_name
+        else:
+            column_mapping[col] = col
+
+    # Apply the renaming using the dictionary
+    renamed_distinct = same_distinct.rename(columns=column_mapping)
+
+    # Create tuples for the MultiIndex
+    index_tuples = list(zip(renamed_distinct.columns[1:], renamed_distinct.iloc[0, 1:].values))
+    final_enhance=renamed_distinct.iloc[1:,:]
+    final_enhance.rename(columns={'Start condition': 'Habitat'}, inplace=True)
+    final_enhance.set_index('Habitat', inplace=True)
+    final_enhance.columns=pd.MultiIndex.from_tuples(index_tuples, names=['Start Condition', 'Target Condition'])
+
+
+    # ---  Get values and reshape ---
+    # The .values will be a 1D array ordered correctly by the new index
+    n_habitats = len(HABITAT_TYPE_TO_ID)
+    conditions = list(CONDITION_CATEGORY_TO_ID.keys())
+    n_conditions = len(conditions)
+    s=np.full((n_habitats, n_conditions, n_conditions), jnp.nan, dtype=np.float32)
+    # Reshape the values into a 3D array 
+    for habitat in HABITAT_TYPE_TO_ID.keys():
+        for start_condition in CONDITION_CATEGORY_TO_ID.keys():
+            try:
+                for tc in final_enhance.loc[habitat, (start_condition,)].index:
+                    # Extract the habitat ID and condition IDs
+                    habitat_id = HABITAT_TYPE_TO_ID[habitat]
+                    current_id= CONDITION_CATEGORY_TO_ID[start_condition]
+                    target_id = CONDITION_CATEGORY_TO_ID[tc]
+                    s[habitat_id, current_id, target_id] = final_enhance.loc[habitat, (start_condition, tc)]
+            except Exception as e:
+                #print(f"Error processing habitat '{habitat}' with start condition '{start_condition}': {e}")
+                continue
+
+
+        #start_condition_id = CONDITION_CATEGORY_TO_ID[start_condition]
+        #target_condition_id = CONDITION_CATEGORY_TO_ID[target_condition]
+        #s[habitat_id, start_condition_id, target_condition_id] = s.values[i]
+
+    # --- 2. Convert to JAX array ---
+    lookup_jax_same = jnp.array(s, dtype=jnp.float32)
+
+    ## ----Different distinctiveness values----
+    diff_distinct=df_temporal_enhance[df_temporal_enhance.columns[0:8]]
+    target=diff_distinct.iloc[0,1:]
+    current=[i.split('Lower Distinctiveness Habitat - ')[1] for i in diff_distinct.iloc[1,1:]]
+    diff_distinct_enhance=diff_distinct.iloc[2:,:]
+    diff_distinct_enhance.rename(columns={'Start condition': 'Habitat'}, inplace=True)
+    diff_distinct_enhance.set_index('Habitat', inplace=True)
+    diff_distinct_enhance.columns=pd.MultiIndex.from_tuples(list(zip(current, target.values.tolist())), names=['Start Condition', 'Target Condition'])
+    ordered_enhance_values = []
+    for habitat_name in HABITAT_TYPE_TO_ID.keys():
+        if habitat_name in diff_distinct_enhance.index.values:
+            # Get the row for this habitat from the loaded CSV, and extract the condition columns
+            row_data = diff_distinct_enhance.loc[habitat_name]
+            # Convert any "30+" strings to numeric 32
+            row_data = row_data.apply(lambda x: 32 if isinstance(x, str) and "30+" in x else x)
+            ordered_enhance_values.append(row_data.values)
+        else:
+            # If a habitat from master list is not in condition table, assume all 0s for conditions
+            # (or raise an error if this should be impossible)
+            ordered_enhance_values.append(jnp.full(len(diff_distinct_enhance.columns),jnp.nan, dtype=jnp.float32))
+    ordered_enhance_values = jnp.array(ordered_enhance_values, dtype=jnp.float32)
+    # Convert the 'Years' column to a JAX array of multipliers
+    return ordered_enhance_values,lookup_jax_same
+
+DISTINCTIVENESS_ENHANCEMENT_TEMPORAL,CONDITION_ENHANCEMENT_TEMPORAL = _load_temporal_enhancement_multiplier()
+
+
 
